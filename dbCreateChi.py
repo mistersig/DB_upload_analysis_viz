e34cf97332dc0d6ebccf657d6b32d0b7ddd3bc08
Sigfrido Gomez <Sigfrido@sigfridos-mbp.attlocal.net>
2018-07-03 10:44:39 -0500

    first commit with all the data
    I added the repository last.... I moved the files from the older older to the new repo to upload and share.

---
+++ dbCreateChi.py
@@ -0,0 +1,141 @@
+import psycopg2 as dbcooper
+import pprint
+import pandas as pd
+#from config import config
+
+def cooperCreate(dbName):
+	''' Initial function to create and load data  '''
+	#connection string to String
+	connString = "host='localhost' user='postgres' password='' "
+	# print the connection string we will use to connect
+	print "Connecting to database\n	->%s" % (connString)
+	#get a connection, exception will be raised here if no connection made
+	conn = dbcooper.connect(connString)
+	print 'connected to db \ncreating ' + dbName
+
+	#needed to create a db psycopg2.extensions.ISOLATION_LEVEL_AUTOCOMMIT
+	conn.set_isolation_level(dbcooper.extensions.ISOLATION_LEVEL_AUTOCOMMIT)
+	#will return object than can be used to query | cursor()
+	queryCon = conn.cursor()
+
+	#execute to CREATE TABLE 
+	queryCon.execute("CREATE DATABASE " + dbName)
+
+	# retrieve records from db 
+	#records = queryCon.fetchall()
+	#close out
+	queryCon.close()
+	conn.close()
+	print "DB %s has been created " %(dbName)
+	print "\n"
+	
+
+	#pprint.pprint(records)
+
+
+def cooperTables(dbName, fName):
+	''' Function to create table and load data  '''
+	connString = "host='localhost' dbname='%s' user='postgres' password='' " %(dbName)
+	# print the connection string we will use to connect
+	print "Connecting to database\n	->%s" % (connString)
+
+	#get a connection, exception will be raised here if no connection made
+	conn = dbcooper.connect(connString)
+
+	#will return object than can be used to query | cursor()
+	queryCon = conn.cursor()
+
+	#create spatial references for point data 
+	queryCon.execute("CREATE EXTENSION postgis")
+	
+	#ID,Case Number,Date,Time,Block,Primary Type,Description,Location Description,Arrest,Domestic,District,Ward,Community Area,FBI Code,Latitude,Longitude
+	queryExpress = '''CREATE TABLE Crime2014q1
+	(
+	ID serial primary key,
+	Case_Number varchar(64),
+	Date date,
+	time time,
+	Block varchar(64),
+	Primary_Type varchar(64),
+	Description varchar(64),
+	Location_Description varchar(64),
+	Arrest boolean,
+	Domestic boolean,
+	District smallint,
+	Ward smallint,
+	Community_Area smallint,
+	FBI_Code varchar(64),
+	Latitude float(8),
+	Longitude float(8)
+	); '''
+
+	#execute query 
+	queryCon.execute(queryExpress)
+	#queryCon.commit()
+	print "TABLE CREATED"
+	print "\n"
+	print "\n"
+
+	conn.commit()
+	queryCon.close()
+
+
+def cooperUpload(fName):
+	connString = "host='localhost' dbname='%s' user='postgres' password='' " %(dbName)
+	# print the connection string we will use to connect
+	print "Connecting to database\n	->%s" % (connString)
+	#get a connection, exception will be raised here if no connection made
+	conn = dbcooper.connect(connString)
+	#will return object than can be used to query | cursor()
+	queryCon = conn.cursor()
+	print "\n"
+	print "\n"
+	#print fName
+	table_name = 'crime2014q1'
+	file_object = fName
+	SQL_STATEMENT = """
+    COPY %s FROM STDIN WITH
+    CSV
+    HEADER
+    DELIMITER AS ','
+    """
+	queryCon.copy_expert(sql=SQL_STATEMENT % table_name, file=file_object)
+	conn.commit()
+	queryCon.close()
+	print "UPLOAD SUCCESS!!!!"
+	print "\n"
+	print "\n"
+
+
+
+
+
+##################
+#Global Variable 
+
+pathFile = '/Users/Sigfrido/Documents/GitHub/ChiCrimeDB14/dbUpload2Graph/data/quarter/'
+
+fileName = pathFile + 'crimes201401_201703.csv'
+
+
+f = open(fileName)
+
+
+
+
+
+#DB NAME
+dbName = 'chicagocrime2014'
+
+
+#create DB
+cooperCreate(dbName)
+#tables 
+cooperTables(dbName,f)
+cooperUpload(f)
+
+f.close()
+
+
+
+
---
+++ queryChi.py
@@ -0,0 +1,161 @@
+import psycopg2 as dbcooper
+import pprint
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import sys
+import datetime
+#from config import config
+
+
+def cooperChanges(dbName, query, update):
+	''' Initial function to update table '''
+	#connection string to String
+	connString = "host='localhost' dbname='%s' user='postgres' password='' " %(dbName)
+	# print the connection string we will use to connect
+	print "Connecting to database\n	->%s" % (connString)
+	print "\n"
+	#get a connection, exception will be raised here if no connection made
+	conn = dbcooper.connect(connString)
+	print 'connected to db \ncreating ' + dbName
+
+	#will return object than can be used to query | cursor()
+	queryCon = conn.cursor()
+	print "Please QA the following"
+	print "\n"
+	print query
+	print update
+	print "\n"
+	approve = raw_input("Are the queries correct? (yes | no) : ").lower()
+
+	if approve == 'yes':
+		print "updating...."
+		pass
+	else:
+		print "Query incorrect please revise. Script aborted"
+		sys.exit()
+
+	queryCon.execute(update)
+
+	#queryCon.execute(query)
+	
+	conn.commit()
+
+	queryCon.close()
+	conn.close()
+	print "Update Complete"
+	print "\n"
+
+
+def cooperVIZ(dbName, query):
+	''' Used map plot for a quick and dity graph '''
+	#connection string to String
+	connString = "host='localhost' dbname='%s' user='postgres' password='' " %(dbName)
+	# print the connection string we will use to connect
+	print "Connecting to database\n	->%s" % (connString)
+	print "\n"
+	#get a connection, exception will be raised here if no connection made
+	conn = dbcooper.connect(connString)
+	#print 'connected to db \ncreating ' + dbName
+
+	#will return object than can be used to query | cursor()
+	queryCon = conn.cursor()
+	queryCon.execute(query)
+	#print query
+
+
+	data = queryCon.fetchall()
+
+	pData = []
+	xTickMarks = []
+
+	for row in data:
+		#print row[1]
+		pData.append(int(row[1]))
+		xTickMarks.append(str(row[0]))
+	#print pData
+
+	#plot for graph 
+	fig = plt.figure()
+	ax = fig.add_subplot(111)
+	#need to work and adjust spacing X labels still get cut off a bit 
+	fig.set_size_inches(8,11.5)
+	fig.autofmt_xdate()
+
+	#necessary variables
+	ind = np.arange(len(pData))    #the x locations 
+	width = 0.20 #width of the bars
+	## the bars
+	rects1 = ax.bar(ind, pData, width,
+                color='black',
+                error_kw=dict(elinewidth=2,ecolor='red'))
+
+	# axes and labels
+	ax.set_xlim(-width,len(ind)+width)
+	#ax.set_xlim()
+	maxV = np.amax(pData)
+	ax.set_ylim(0,maxV)
+	#Labels
+	ax.set_ylabel('Counts')
+	ax.set_xlabel('Crimes')
+	ax.set_title('Crimes Commited by Count Jan 14 - Mar 14')
+
+	ax.set_xticks(ind+width)
+	xtickNames = ax.set_xticklabels(xTickMarks)
+	plt.setp(xtickNames, rotation=45, fontsize=10)
+
+
+	plt.show()
+	#print data 
+	#conn.commit()
+	queryCon.close()
+	conn.close()
+	print "Graph Complete"
+	print "\n"
+
+	
+
+
+
+######## MAIN ##########
+
+#DB NAME
+dbName = 'chicagocrime2014'
+
+# variables to change and update
+
+
+primary_type = 'Grand Theft Auto'
+arrest = 'FALSE'
+
+#for cooper changes
+sqlQuery = "select * from crime2014q1 where Primary_Type = 'MOTOR VEHICLE THEFT' and arrest = false limit 10;"
+#for cooper changes 
+sqlUpdate = """update crime2014q1 set Primary_Type = '%s' where Primary_Type = 'MOTOR VEHICLE THEFT' and arrest = false;""" %(primary_type)
+#print sqlUpdate #test
+
+#query for VIZ
+sqlQViz = """select distinct primary_type,count(primary_type) from crime2014q1 group by primary_type order by count desc;"""
+
+
+
+cooperChanges(dbName,sqlQuery,sqlUpdate)
+
+cooperVIZ(dbName,sqlQViz)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
